e4a899aed5d25ddfbadd8e65f1740f640ae408f8
PedroSantiago97 <pedro.santiago8051@outlook.com>
2023-09-25 19:58:25 -0300

    Update exe.py

---
+++ Exercício 3/exe.py
@@ -0,0 +1,9 @@
+valor1 = input("Digite algum valor: ")
+valor2 = input("Digite algum valor: ")
+
+vint1 = float(valor1)
+vint2 = float(valor2)
+
+
+soma = vint1 + vint2
+print("A soma dos valores é: %s!" %(soma))
