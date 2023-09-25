cb604e2658faa690a806fb0b43ef5f908d7b9609
PedroSantiago97 <pedro.santiago8051@outlook.com>
2023-09-25 20:02:44 -0300

    att

--- Exercício 3/exe.py
+++ Exercício 3/exe.py
@@ -7,3 +7,14 @@ vint2 = float(valor2)
 
 soma = vint1 + vint2
 print("A soma dos valores é: %s!" %(soma))
+
+# Assim tbm resolve
+'''
+valor1 = float(input("Digite algum valor: "))
+valor2 = float(input("Digite algum valor: "))
+
+soma = valor1 + valor2
+print("A soma dos valores é: %s!" %(soma))
+'''
+
+# Colocando o Float na frente direto
