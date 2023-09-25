44355cee983c43acc33fe077e891f198c3b47a63
PedroSantiago97 <pedro.santiago8051@outlook.com>
2023-09-25 20:56:14 -0300

    att

--- Exercício 4/exe.py
+++ Exercício 4/exe.py
@@ -2,12 +2,13 @@ obj = input("Digite algo: ")
 
 print("O tipo primitivo desse valor é: %s!" %type(obj))
 print("Só tem espaços? %s!" %(obj.isspace())) # .isspace: Indentifica espaços no objeto
-print("É um número? %s!" %(obj.isnumeric())) #
-print("É alfabético? %s!" %(obj.isalpha()))
-print("É alfanumérico %s!" %(obj.isalnum()))
-print("Está em maiúsculas? %s!" %(obj.isupper()))
-print("Está em minúsculas? %s!" %(obj.islower()))
-print("Está capitalizada? %s!" %(obj.istitle()))
+print("É um número? %s!" %(obj.isnumeric())) # .isnumeric: Indentifica se o objeto é um numero
+print("É alfabético? %s!" %(obj.isalpha())) # .isalpha: Indentifica se o objeto possui letras
+print("É alfanumérico %s!" %(obj.isalnum())) # .isalnum: Indentifica se o objeto possui letras e numeros
+print("Está em maiúsculas? %s!" %(obj.isupper())) # .isupper: Indentifica se o objeto esta em Maiusculas
+print("Está em minúsculas? %s!" %(obj.islower())) # .islower: Indentifica se o objeto esta em Minusculas
+print("Está capitalizada? %s!" %(obj.istitle())) # .istitle: Indentifica se o objeto esta em maiusculas ou minusculas
+
 
 
 
