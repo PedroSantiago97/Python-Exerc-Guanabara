a53e3b1ace831ae8146b3820b7301db34c8c0a00
PedroSantiago97 <pedro.santiago8051@outlook.com>
2023-09-26 07:49:47 -0300

    att

--- Exercício 7/exe.py
+++ Exercício 7/exe.py
@@ -1,1 +1,6 @@
+pr = float(input("Primeira nota do aluno: "))
+sg = float(input("Segunda nota do aluno: "))
 
+print(f"A média entre {pr:.1f} e {sg:.1f} é {((sg+pr)/2):.1f}")
+
+# O parâmetro :.1f determina quantos numeros terei depois da virgula no objeto em que foi posto. O número depois do f representa a quantidade de termos depois da virgula
---
+++ Exercício 8/exe.py
@@ -0,0 +1,12 @@
+dis = int(input("Digite uma distância em metros: "))
+print(f"A medidade de {dis} m corresponde a:")
+print(f"{dis/1000}km")
+print(f"{dis/100}hm")
+print(f"{dis/10}dam")
+print(f"{dis*10}dm")
+print(f"{dis*100}cm")
+print(f"{dis*1000}mm")
+
+# A conversão que é feita por {valor}x10^-2 por exemplo pode ser feita com {valor}/100
+# Lembrar de pensar fora do seu pensamento comum
+# Procurar soluções simples e agéis
