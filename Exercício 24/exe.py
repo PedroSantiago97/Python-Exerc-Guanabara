f5ab0533e588dcfd457903b39b3f04b10e3aafe2
PedroSantiago97 <pedro.santiago8051@outlook.com>
2023-09-27 18:29:17 -0300

    ATT

--- Exercício 24/exe.py
+++ Exercício 24/exe.py
@@ -1,1 +1,6 @@
+cid = input('Em que cidade você nasceu? ')
+print(cid[:5].upper() == 'SANTO')
 
+# Exercicio de fixação ultilizado pra fixar o senso critico do programador
+# O codigo tinha o objetivo de Ver se a primeira palavra na resposta é Santo
+# A resposta dada foi modificada para se enquadrar nos parametros prorpostos pelo exercicio
