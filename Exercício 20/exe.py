ba534211743a227da2ecfc9dc2f416bc3f2dca06
PedroSantiago97 <pedro.santiago8051@outlook.com>
2023-09-27 11:31:12 -0300

    attt

--- Exercício 20/exe.py
+++ Exercício 20/exe.py
@@ -1,1 +1,16 @@
+import random
 
+
+pri = input("Primeiro Aluno: ")
+seg = input("Segundo Aluno: ")
+ter = input("Terceiro Aluno: ")
+qua = input("Quarto Aluno: ")
+
+ordem = [pri, seg, ter, qua]
+random.shuffle(ordem)
+print(f"A ordem de apresentação será: ")
+print(ordem)
+
+# Lista embaralhada
+# random.shuffle(): Embaralha um array
+# Usar o random.shuffle() antes do print(f) e mostrar a variável, não funciona dentro do print(f) T.T
