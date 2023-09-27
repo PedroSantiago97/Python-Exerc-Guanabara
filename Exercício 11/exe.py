858c56aec13485380e39ff4ed3973ec7b6a4d6ae
PedroSantiago97 <pedro.santiago8051@outlook.com>
2023-09-27 08:24:54 -0300

    att

--- Exercício 11/exe.py
+++ Exercício 11/exe.py
@@ -1,1 +1,7 @@
+lar = float(input("Largura da parede: "))
+alt = float(input("Altura da parede: "))
 
+print(f"Sua parede tem a dimensão de {lar} x {alt} e sua aréa é de {(lar*alt):.3f}m².")
+print(f"Para pinntar essa parede, você precisará de {((lar*alt)/2):.4f}L de tinta.")
+
+# Poderia colocar tbm print(''.format())
--- Exercício 12/exe.py
+++ Exercício 12/exe.py
@@ -1,1 +1,2 @@
-
+pre = float(input("Qual é o  preço do produto? R$"))
+print(f"O produto que custava R${pre}, na promoção com desconto de 5% vai custar R${(pre*0.95):.2f}")
