b3ea98fe83210bf570e3d4c831ec2178165859f5
PedroSantiago97 <pedro.santiago8051@outlook.com>
2023-09-28 09:53:19 -0300

    Attt

--- Exercicios 1-100/exe26.py
+++ Exercicios 1-100/exe26.py
@@ -1,4 +1,8 @@
-frase = input("Digite uma frase: ").upper()
-
+frase = input("Digite uma frase: ").upper().strip()
 
 print(f"A letra A aparece {frase.count('A')} vezes na frase.")
+print(f"A primeira vez que e letra A aparece é na posição: {frase.find('A')+1} ")
+print(f"A ultima vez que e letra A aparece é na posição: {(frase.rfind('A')+1)-(frase.count(' '))}")
+
+# Usuário é a pior raça que tem, ele vai fazer de tudo pro seu programa nao funcionar - Guanabara, Gustavo
+# Mais exercicios de fixação sobre os parametros .find(), .count(), .upper() e .strip()
--- Exercicios 1-100/exe27.py
+++ Exercicios 1-100/exe27.py
@@ -1,3 +1,13 @@
+nome = (input('Digite seu nome completo: '))
+
+separado = nome.split(' ')
+# Sempre que usar .split() criar uma variável pra criar a varipavel modificada
+
+print(f'Muito prazer em te conhecer!')
+print(f'Seu primeiro nome é {separado[0]}')
+print(f'Seu ultimo nome é {separado[len(separado)-1]}')
+
+# Sempre que mecher com Arrays se atentar ao fato da sequencia começar no 0 e o .len() começa a contar do 1 !!! 
 
 
-    
+
