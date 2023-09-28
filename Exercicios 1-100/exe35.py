fe235a4c03821e436d70738159ab51380aba2ba8
PedroSantiago97 <pedro.santiago8051@outlook.com>
2023-09-28 18:04:30 -0300

    Atualizacioneee

--- Exercicios 1-100/exe35.py
+++ Exercicios 1-100/exe35.py
@@ -1,2 +1,17 @@
+print('-='*20)
+print('Analisador de Triângulos')
+print('-='*20)
 
+ps = float(input('Primeiro segmento: '))
+ss = float(input('Segundo segmento: '))
+ts = float(input('Terceiro segmento: '))
+
+if ps + ss > ts or ss + ts > ps or ts + ps > ss:
+    print('Os segmentos PODEM formar um triangulo')    
+else:
+    print('Os segmentos NÃO PODEM formar um triangulo')
+
+# Exemplo de condicional com or
+# Nessa logica, nao posso ter um triangulo se dois dos segmentos somados nao forem maior que o segmento que resta
+# Ex: Primeiro segmento + Terceiro segmento forem Menor que o Segundo segmento
 
---
+++ Exercicios 1-100/exe36.py
@@ -0,0 +1,17 @@
+import time
+from colorama import Fore, Style, init
+init()
+
+casa = float(input('Valor da casa: R$'))
+sal = float(input('Salário do comprador: R$'))
+pre = int(input('Quantos anos de financiamento: '))
+
+par = pre*12
+
+print(f'Para comprar uma casa de R${casa:.2f} a prestação será de R${casa/par:.2f}')
+if casa/par > sal*0.3:
+    
+    print(f'Emprestimo {Fore.RED}NEGADO{Style.RESET_ALL}')
+elif casa/par < sal*0.3:
+    
+    print(f'Emprestimo {Fore.GREEN}CONCEDIDO{Style.RESET_ALL}')
