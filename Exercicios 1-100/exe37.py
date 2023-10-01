87456e20cf528cc8f414c80eccb9845fdadc968d
PedroSantiago97 <pedro.santiago8051@outlook.com>
2023-10-01 05:30:11 -0300

    att

--- Exercicios 1-100/exe37.py
+++ Exercicios 1-100/exe37.py
@@ -1,2 +1,23 @@
+num = int(input('Digite um número inteiro: '))
+
+print('Escolha uma das bases para conversão')
+print('[1] converter para BINÁRIO')
+print('[2] converter para OCTAL')
+print('[3] converter para HEXADECIMAL')
+
+op = int(input('Sua Opção: '))
+
+if op == 1:
+    print(f'O número {num} convertido para BINÁRIO é igual a {bin(num)[2:]}')
+elif op == 2:
+    print(f'O número {num} convertido para OCTAL é igual a {oct(num)[2:]}')
+else:
+    print(f'O número {num} convertido para HEXADECIMAL é igual a {hex(num)[2:]}')
+
+# Um adeno a operação de "Fatiar" em que [2:] fatia os resultados mostrando eles apartir do terceiro termo
+# Pois os 2 primeiros foram fatiados !!!!
+
+
+
 
 
--- Exercicios 1-100/exe38.py
+++ Exercicios 1-100/exe38.py
@@ -1,2 +1,12 @@
+pr = int(input('Primeiro número: '))
+sg = int(input('Segundo número: '))
 
+if pr > sg:
+    print('O primeiro valor é MAIOR')
+elif sg > pr:
+     print('O Segundo valor é MAIOR')
+else:
+    print('Os valores são IGUAIS')
 
+   
+ 
