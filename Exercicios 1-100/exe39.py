3b24c65559d46114c17b6d0ce2513741df5cf992
PedroSantiago97 <pedro.santiago8051@outlook.com>
2023-10-02 09:16:24 -0300

    Att

--- Exercicios 1-100/exe39.py
+++ Exercicios 1-100/exe39.py
@@ -1,2 +1,24 @@
+import datetime
+import time
+
+anonasc = int(input('Ano de nascimento: '))
+ano_atual = datetime.datetime.now().year
+
+print('ANALISANDO...')
+time.sleep(0.8)
+
+
+if (ano_atual - anonasc) < 18:
+    print(f'Quem nasceu em {anonasc} tem {ano_atual - anonasc} anos em {ano_atual}. ')
+    print(f'Ainda faltam {18 - (ano_atual - anonasc)} anos para o alistamento.')
+    print(f'Seu alistamento será em {anonasc + 18}')
+elif ano_atual - anonasc > 18:
+    print(f'Quem nasceu em {anonasc} tem {ano_atual - anonasc} anos em {ano_atual}. ')
+    print(f'Você já deveria ter se alistado a {ano_atual - (anonasc + 18)} anos.')
+    print(f'Seu alistamento foi em {anonasc + 18}.')
+else:
+    print(f'Quem nasceu em {anonasc} tem {ano_atual - anonasc} em {ano_atual}.')
+    print('Você tem que se alistar IMEDIATAMENTE!')
+
 
 
--- Exercicios 1-100/exe40.py
+++ Exercicios 1-100/exe40.py
@@ -1,2 +1,14 @@
+pr = float(input('Primeira nota: '))
+sg = float(input('Segunda nota: '))
 
+media = (pr + sg) / 2
+print(f'Tirando {pr} e {sg}, a média do aluno é {media:.1f}')
+if media > 6:  
+    print('O aluno está APROVADO!')
+elif media <= 6.9 and media >= 5.0:   
+    print('O aluno está de RECUPERAÇÃO!')
+elif media < 5:
+    print('O aluno está REPROVADO!')
 
+# Um adeno a produção de codigo limpo
+# Se houver necessidade de repetir o codigo nao precisa colocar no if !!!!
