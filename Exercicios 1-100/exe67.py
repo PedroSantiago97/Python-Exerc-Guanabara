d566b60c11caa45a9241315bd492da9bfc54db79
PedroSantiago97 <pedro.santiago8051@outlook.com>
2023-10-06 17:11:15 -0300

    attss

--- Exercicios 1-100/exe67.py
+++ Exercicios 1-100/exe67.py
@@ -1,2 +1,12 @@
-
+c=0
+while True:
+    num = int(input('Quer ver a tabuada de qual valor? '))
+    if num >= 0:
+        print('-'*15)
+        for c in range(0, 11):
+            print(f'{num}  X {c:2} = {c*num}')
+        print('-'*15)    
+    else:
+        print('Fim')
+        break
 
--- Exercicios 1-100/exe68.py
+++ Exercicios 1-100/exe68.py
@@ -1,2 +1,25 @@
+import random
+
+print('=-'*20)
+print('       VAMOS JOGAR PAR OU ÍMPAR !')
+
+
+while True:
+    print('=-'*20)
+    numero_aleatorio = random.randint(0, 10)
+    valor = int(input('Digite um valor: '))
+    op = input('Par ou Impar? [P/I]: ')
+    resultado = int(valor + numero_aleatorio)
+    print('-'*20)
+    print(f'Você jogou {valor} e o computador {numero_aleatorio}. Total de {valor+numero_aleatorio}, deu {par_ou_impar}')
+    print('-'*20)
+    
+   
+      
+
+
+
+
+
 
 
