cd598022937201daba58b9a7667a569ce2146ee5
PedroSantiago97 <pedro.santiago8051@outlook.com>
2023-10-07 09:43:13 -0300

    att

--- Exercicios 1-100/exe68.py
+++ Exercicios 1-100/exe68.py
@@ -3,16 +3,34 @@ import random
 print('=-'*20)
 print('       VAMOS JOGAR PAR OU ÍMPAR !')
 
-
+t=0
 while True:
     print('=-'*20)
     numero_aleatorio = random.randint(0, 10)
     valor = int(input('Digite um valor: '))
     op = input('Par ou Impar? [P/I]: ')
     resultado = int(valor + numero_aleatorio)
-    print('-'*20)
-    print(f'Você jogou {valor} e o computador {numero_aleatorio}. Total de {valor+numero_aleatorio}, deu {par_ou_impar}')
-    print('-'*20)
+    if resultado % 2 == 0:
+        parouimpar = 'PAR'
+    else:
+        parouimpar = 'IMPAR'
+    print('-'*50)
+    print(f'Você jogou {valor} e o computador {numero_aleatorio}. Total de {valor+numero_aleatorio}, deu {parouimpar}')
+    print('-'*50)   
+    if resultado % 2 == 0 and op.upper() == 'P'or resultado % 2 != 0 and op.upper() == 'I':
+        print(f'Você VENCEU!')
+        print('Vamos joguar novamente...')
+        t+=1
+    else:
+        print(f'Você PERDEU!')
+        print('-'*50) 
+        print(f'GAME OVER!, você venceu {t} vezes')
+        break
+    
+
+
+    
+
     
    
       
--- Exercicios 1-100/exe69.py
+++ Exercicios 1-100/exe69.py
@@ -1,2 +1,31 @@
+listanome = []
+listaidade = []
+listasexo = []
+
+while True:
+    print('-'*30)
+    print('     CADASTRE UMA PESSOA')
+    print('-'*30)
+    nome = input('Nome: ')
+    idade = input('Idade: ')
+    sexo = input('Sexo: [M/F] ')
+    print('-'*30)
+    op = str(input('Quer continuar? [S/N] '))
+    listanome.append(nome)
+    listaidade.append(idade)
+    listasexo.append(sexo)
+
+    if op.upper() != 'S':
+        break
+
+    
+print(listaidade)
+print(listasexo)
+print(listanome)
+
+
+
+    
+
 
 
